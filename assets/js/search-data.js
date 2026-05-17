9c186d4d410a40bbae32ea4268a941ec55fc740d
ShaziaIshaq <157230380+ShaziaIshaq@users.noreply.github.com>
2026-05-17 08:06:29 +0000

    Deploying to gh-pages from @ ShaziaIshaq/ShaziaIshaq.github.io@4f26e06e55f93ca5bb796dc171854686ad66a9a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,21 +489,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Shazia_Ishaq_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%73%68%61%7A%69%61.%69%73%68%61%71@%65%6D%61%69%6C.%75%6E%69-%66%72%65%69%62%75%72%67.%64%65", "_blank");
         },
       },{
         id: 'social-rss',
@@ -517,14 +510,28 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=bBTp_nkAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0009-0006-2800-6238", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/shazia-ishaq", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/ShaziaIshaq", "_blank");
         },
       },{
       id: 'light-theme',
